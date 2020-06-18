5531ae408aa3b003a207f3fe0ca5eebd24fd3a3f
Kathlyn Purcell <64290264+kathlynpurcell@users.noreply.github.com>
2020-06-17 20:44:23 -0400

    Add files via upload
    first copy

---
+++ usinggit.py
@@ -0,0 +1,10 @@
+'''
+Im working on learning git command line to collaborate
+this is just a simple program so i can add to it, share it
+    upload and download it and learn git without messing up a real program
+'''
+print('We can add two numbers!\n')
+num1 = int(input("First number: "))
+num2 = int(input("\nSecond number: "))
+added = num1+num2
+print("\n The result of %i + %i is %i. Thanks for using!" %(num1,num2,added))
