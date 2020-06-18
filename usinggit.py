2a78d1386a17550bc5e9a6665d852a2250ce76fd
kathlynpurcell <64290264+kathlynpurcell@users.noreply.github.com>
2020-06-17 20:57:21 -0400

    adding to the program through windows

--- usinggit.py
+++ usinggit.py
@@ -7,4 +7,14 @@ print('We can add two numbers!\n')
 num1 = int(input("First number: "))
 num2 = int(input("\nSecond number: "))
 added = num1+num2
-print("\n The result of %i + %i is %i. Thanks for using!" %(num1,num2,added))
+print("\n The result of %i + %i is %i." %(num1,num2,added))
+
+yn = int(input('\n Do you want to add a third number? Press 1 for yes and 0 for no.'))
+
+if yn ==1:
+    num3 = int(input("\n Third Number: "))
+    added2 = added+ num3
+    print("\n The result of %i + %i + %i is %i" %(num1,num2,num3,added2))
+
+print("\n Thanks for using! \n")
+
